4de96d76bf15516995260441cf50ee1c23cb73ca
Guilherme <guilherme.caleman@gmail.com>
2021-05-26 19:35:15 +0100

    about section

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,11 +1,27 @@
 import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
 import './AboutMe.css';
 
 export const AboutMe = () => {
     return (
-        <div className="AboutMe" id="about">
-            <h1>About Me</h1>
-            <p>A little about me</p>
+        <div className="aboutMe" id="about">
+            <Container className="about-container">
+                <Row>
+                    <Col md={4} className="col-logo">
+
+                    </Col>
+                    <Col md={8} className="col-border">
+                        <h1>Guilherme Caleman</h1>
+                        <p>
+                            I'm a <span>first-class honors</span> IT graduate with background in Mechanical Engineering.<br />
+                My knowledge in mechanical engineering and experience working in a fast paced environments, together with my science in computing skills enhance my <span>problem solving abilities</span> and my capacity to <span>work in a group</span> to achieve results. <br />
+                My goal is to evolve in my career as a <span>Frontend Developer</span> and to work in DevOps in the future. I believe that my <span>communication</span>, <span>collaboration</span> and <span>problem solving skills</span> matches the necessary abilities for a succesful career in the field. <br />
+                In my free time I enjoy watching sports <span>(specially team sports)</span> and movies, playing football, trying to cook <span>(not my best skill I got to say..)</span> and traveling.
+                        </p>
+                    </Col>
+                </Row>
+            </Container>
+            <hr></hr>
         </div>
     )
 }
--- src/components/Education.js
+++ src/components/Education.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
 import './Education.css';
 
 export const Education = () => {
     return (
-        <div id="education" className="Education">
-            <h1>Education</h1>
-            <p>My education</p>
+        <div id="education" className="education">
+            <Container className="education-container">
+                <Row>
+                    <Col md={8} className="col-border">
+                        <h1>Education</h1>
+
+                    </Col>
+                    <Col md={4} className="col-logo">
+
+                    </Col>
+                </Row>
+            </Container>
+            <hr></hr>
         </div>
     )
 }
